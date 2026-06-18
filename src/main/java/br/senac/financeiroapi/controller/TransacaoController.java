2f967a0dd755c0ce2d4def8c148296669ef7e23f
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:03:24 -0300

    Update TransacaoController.java

--- src/main/java/br/senac/financeiroapi/controller/TransacaoController.java
+++ src/main/java/br/senac/financeiroapi/controller/TransacaoController.java
@@ -16,12 +16,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/transacoes")
 @Tag(name = "Transações", description = "CRUD de transações financeiras")
+@CrossOrigin("*")    
 public class TransacaoController {
 
     private final TransacaoService transacaoService;
